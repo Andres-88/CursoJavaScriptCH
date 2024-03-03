2341f8bd1357f39de4de73e00305a545e4a1b73c
Andres-88 <andresuli994@gmail.com>
2024-03-03 16:32:48 -0300

    Se cambió el nombre del juego 2 a Ruleta Rusa y se creó el juego 3 Piedra Papel o Tijera

--- js/main.js
+++ js/main.js
@@ -29,7 +29,7 @@ function juego1(){
     }
 }
 
-// Juego Sorteo
+// Juego Ruleta Rusa
 function juego2(){
     const hasta = 10;
             let intento = -1;              
@@ -53,11 +53,108 @@ function juego2(){
             }
 }
 
+// Piedra, Papel o Tijera
+function juego3(nombre){
+    let ppt;
+    let cpu;
+    let pptEsp;
+    let cpuEsp;    
+    let puntajeUsuario = 0;
+    let puntajeCPU = 0;
 
+    for (let i = 0; i < 3; i++) {   
+
+        cpu = Math.floor((Math.random() * 3) + 1);
+        ppt = parseInt(prompt("¡Piedra, papel o tijera 1 2 3!\n1: Piedra\n2: Papel\n3: Tijera"));
+
+        if(cpu == 1){
+            cpuEsp = "Piedra";
+        } else if (cpu == 2) {
+            cpuEsp = "Papel";
+        } else {
+            cpuEsp = "Tijera";
+        }
+
+        if(ppt == 1){
+            pptEsp = "Piedra";
+        } else if (ppt == 2) {
+            pptEsp = "Papel";
+        } else {
+            pptEsp = "Tijera";
+        }
+
+        alert(nombre + ": " + pptEsp + "\nCPU: "+ cpuEsp);
+
+        if((puntajeCPU == 0 && puntajeUsuario == 0) && ((cpu == 1 && ppt == 2) || (cpu == 2 && ppt == 3) || (cpu == 3 && ppt == 1))){
+            puntajeUsuario = puntajeUsuario + 1;
+            alert("¡Punto para vos!\n" + nombre+ ": " + puntajeUsuario+"\nCPU: " + puntajeCPU + "\n...¿Dos de tres?");
+            
+        }
+
+        if((puntajeCPU != 0 && puntajeUsuario != 0) && ((cpu == 1 && ppt == 2) || (cpu == 2 && ppt == 3) || (cpu == 3 && ppt == 1))){
+            puntajeUsuario = puntajeUsuario + 1;
+            alert(nombre + ": " + puntajeUsuario + "\nCPU: " + puntajeCPU);
+        }
+
+        if((ppt == 1 && cpu == 2) || (ppt == 2 && cpu == 3) || (ppt == 3 && cpu == 1)){
+            puntajeCPU = puntajeCPU + 1;
+            alert(nombre + ": " + puntajeUsuario + "\nCPU: " + puntajeCPU);
+        }
+
+        if((ppt == cpu)){
+            alert("¡Otra vez!");
+
+            while (ppt == cpu){
+                cpu = Math.floor((Math.random() * 3) + 1);
+                ppt = parseInt(prompt("¡Piedra, papel o tijera 1 2 3!\n1: Piedra\n2: Papel\n3: Tijera"));
+
+                if(cpu == 1){
+                    cpuEsp = "Piedra";
+                } else if (cpu ==2) {
+                    cpuEsp = "Papel";
+                } else {
+                    cpuEsp = "Tijera";
+                }
+        
+                if(ppt == 1){
+                    pptEsp = "Piedra";
+                } else if (ppt == 2) {
+                    pptEsp = "Papel";
+                } else {
+                    pptEsp = "Tijera";
+                }
+
+                alert(nombre + ": " + pptEsp + "\nCPU: "+ cpuEsp);
+
+                if (ppt == cpu){
+                    alert("¡Otra vez!");
+                }
+            }
+
+            if((cpu == 1 && ppt == 2)|| (cpu == 2 && ppt == 3) || (cpu == 3 && ppt == 1)){
+                puntajeUsuario = puntajeUsuario + 1;
+            }else{
+                puntajeCPU = puntajeCPU + 1;
+            }
+            
+            alert(nombre + ": " + puntajeUsuario + "\nCPU: " + puntajeCPU);         
+        }
+     
+        if(puntajeUsuario == 2 || puntajeCPU == 2){
+            i = 9; //Valor aleatorio para salir del for. No se Si break hubiese servido.
+        }
+    }
+
+    if(puntajeUsuario > puntajeCPU){
+        alert("Tuviste suerte...\n¡Ganaste!");
+    } else {
+        alert ("Gané\n¡Soy el mejor!");
+    }
+}
 
 // ---------------------------MAIN---------------------------
 
-let opcion = parseInt(prompt("Ingrese tipo de juego: \n 1- Tablas\n 2- Sorteo"));
+let opcion = parseInt(prompt("Ingrese tipo de juego: \n 1- Tablas\n 2- Ruleta Rusa\n 3- Piedra, papel o tijera"));
 
 switch(opcion){
     case 1:
@@ -72,4 +169,10 @@ switch(opcion){
 
     break;
 
+    case 3:
+
+        let jugador = prompt("Ingresá tu nombre:");
+        juego3(jugador);
+
+    break;
 }
